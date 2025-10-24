0421c7d3567ebf8b5d11a4747073d68f30d80110
binggwa <qudrhks317@gmail.com>
2025-10-24 17:02:18 +0900

    feat(App.js) : 입력 구현, 레이스 결과 출력 임시 구현
    입력 구현
    - MissionUtils를 이용해 자동차 이름과 시도할 횟수 입력을 받아온다
    - 실행 결과 포맷에 맞게 콘솔문구 설정
    레이스 결과 출력 임시 구현
    - 레이스를 시작한다
    - 매 사이클마다 자동차 이름과 위치를 출력한다
    - 최종 우승자를 출력한다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import Race from './Race.js';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const namesInput = await MissionUtils.Console.readLineAsync(
+        '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+      );
+
+      const cycleCount = await MissionUtils.Console.readLineAsync(
+        '시도할 횟수는 몇 회인가요?\n',
+      );
+
+      MissionUtils.Console.print('\n실행 결과');
+
+      const race = Race.start(names);
+
+      // 중간 결과 출력
+      race.printEachCycle(cycleCount);
+
+      // 최종 우승자 출력
+      race.printWinners();
+    } catch (error) {
+      MissionUtils.Console.print(error.message);
+      throw error;
+    }
+  }
 }
 
 export default App;
