405d4305d672e30db647b4f96981fd727f7b3982
binggwa <qudrhks317@gmail.com>
2025-10-24 17:52:25 +0900

    feat(App.js) : 자동차 이름 입력 ERROR 케이스 추가
    - 공백문자밖에 없을 경우 [ERROR] 참가할 자동차가 없습니다! 문구 출력

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ class App {
         '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
       );
 
+      const trimmedNames = namesInput.trim();
+      if (trimmedNames.length === 0) {
+        throw new Error('[ERROR] 참가할 자동차가 없습니다!');
+      }
+
       const cycleCount = await MissionUtils.Console.readLineAsync(
         '시도할 횟수는 몇 회인가요?\n',
       );
